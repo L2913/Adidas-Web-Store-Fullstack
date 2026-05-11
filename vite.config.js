c0c47dd5df16355bc912fb344342c33714314557
L2913 <johneljo29@gmail.com>
2026-05-11 09:35:22 +0800

    Fix Azure deployment

---
+++ vite.config.js
@@ -0,0 +1,16 @@
+import { defineConfig } from "vite";
+import { resolve } from "path";
+
+export default defineConfig({
+  build: {
+    rollupOptions: {
+      input: {
+        main: resolve(__dirname, "index.html"),
+        login: resolve(__dirname, "login.html"),
+        register: resolve(__dirname, "register.html"),
+        cart: resolve(__dirname, "cart.html"),
+        orders: resolve(__dirname, "orders.html")
+      }
+    }
+  }
+});
